8f7b8c415f74675af1e7e50c1b78a5308a404dff
harlo <harlo.holmes@gmail.com>
2015-02-17 12:04:12 -0500

    build scripts

---
+++ setup.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+# example: dutils/setup.sh compass_express /path/to/config.json
+
+function run_docker_routine {
+	chmod +x .routine.sh
+	./.routine.sh
+	rm .routine.sh
+}
+
+function do_exit {
+	deactivate venv
+	exit
+}
+
+DUTILS_PKG_ROOT=$1
+
+# Create virtualenv
+virtualenv venv
+source venv/bin/activate
+
+# Install python requirements
+pip install -r dutils/requirements.txt
+
+# Run Docker init
+declare -a D_ROUTINES=("init $2" "build" "commit")
+for DR in "${D_ROUTINES[@]}"; do
+	python $DUTILS_PKG_ROOT $DR
+	if ([ $?  -eq 0 ]); then
+		run_docker_routine
+	else
+		echo "FAILED."
+		do_exit
+	fi
+done
+
+cp dutils/stop.sh .
+chmod +x run.sh
+chmod +x shutdown.sh
+
+do_exit
---
+++ stop.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+echo "Shutting down Compass.  Save state? [y|N]: "
+read $SAVE_STATE
+
+./shutdown.sh $SAVE_STATE
